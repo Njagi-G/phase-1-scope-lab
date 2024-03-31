fcf25eb83a84fd5ab88e62776a5dc9c86fbb45c1
Njagi Githaiga <njagigithaiga@gmail.com>
2024-03-31 04:58:56 +0300

    Submitting scope lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+
+//Instruction 1:
+
+var customerName = 'bob'; 
+
+const leastFavoriteCustomer = 'Dean Martin';
+
+
+//Instruction 2:
+
+function upperCaseCustomerName () {
+
+    customerName ='BOB';
+}
+
+
+//Instruction 3:
+
+function setBestCustomer () {
+
+    bestCustomer='not bob'
+}
+
+bestCustomer;
+
+
+//Instruction 4
+
+function overwriteBestCustomer () {
+
+    return bestCustomer = 'maybe bob';
+}
+
+
+//Instruction 5:
+
+function changeLeastFavoriteCustomer () {
+
+    return leastFavoriteCustomer = 'Elvis Presley';
+}
